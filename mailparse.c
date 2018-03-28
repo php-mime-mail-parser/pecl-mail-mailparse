ea0896639358f85989bd8b61bda924a2a2a0d477
Vincent Dauce <eXorus@users.noreply.github.com>
2018-03-28 06:47:39 +0200

    Update mailparse.c

--- mailparse.c
+++ mailparse.c
@@ -29,11 +29,6 @@
 #include "main/php_output.h"
 #include "php_open_temporary_file.h"
 
-/* just in case the config check doesn't enable mbstring automatically */
-#if !HAVE_MBSTRING
-#error The mailparse extension requires the mbstring extension!
-#endif
-
 #define MAILPARSE_DECODE_NONE		0		/* include headers and leave section untouched */
 #define MAILPARSE_DECODE_8BIT		1		/* decode body into 8-bit */
 #define MAILPARSE_DECODE_NOHEADERS	2		/* don't include the headers */
